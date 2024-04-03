4ad529a9d9da6ebb69adba534acf9c379c5edbf7
Marcin Słomka <m.slomka@student.po.edu.pl>
2024-04-03 14:50:19 +0200

    ImportWindow added validation for csv file, added but not finished method in ServiceDocumentDataBase

---
+++ Class/ServiceDocumentDataBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Profisys_Zadanie.Class
+{
+    public class ServiceDocumentDataBase
+    {
+        private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
+
+        public static async Task InsertDocumentsAsync(List<Document> documents)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        foreach (var document in documents)
+                        {
+                            var command = new SqlCommand("INSERT INTO Documents (Id, Type, Date, FirstName, LastName, City) VALUES (@Id, @Type, @Date, @FirstName, @LastName, @City)", connection, transaction);
+                            command.Parameters.AddWithValue("@Id", document.Id);
+                            command.Parameters.AddWithValue("@Type", document.Type ?? (object)DBNull.Value);
+                            command.Parameters.AddWithValue("@Date", document.Date);
+                            command.Parameters.AddWithValue("@FirstName", document.FirstName ?? (object)DBNull.Value);
+                            command.Parameters.AddWithValue("@LastName", document.LastName ?? (object)DBNull.Value);
+                            command.Parameters.AddWithValue("@City", document.City ?? (object)DBNull.Value);
+
+                            await command.ExecuteNonQueryAsync();
+                        }
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
--- ImportExport/ImportWindow.xaml.cs
+++ ImportExport/ImportWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Profisys_Zadanie.Class;
+using Microsoft.Win32;
 
 namespace Profisys_Zadanie.ImportExport
 {
@@ -26,27 +27,59 @@ namespace Profisys_Zadanie.ImportExport
     public partial class ImportWindow : Window
     {
         public List<Document> Documents = new List<Document>();
+        private string selectedDocumentCsvPath;
+        private string selectedPositionsCsvPath;
 
         public ImportWindow()
         {
             InitializeComponent();
         }
 
-        private void ExitAppBtn_Click(object sender,  RoutedEventArgs e)
+        private void SelectDocumentButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                selectedDocumentCsvPath = openFileDialog.FileName;
+                // Aktualizuj Label z wybraną ścieżką
+                
+                if (ValidateCsvFile(selectedDocumentCsvPath, isDocument: true))
+                {
+                    MessageBox.Show("Plik został poprawnie załadowany!", "Poprawny format", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Niepoprawnie wczytany plik!", "Błąd formatu", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                }
+            }
         }
 
-        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) 
+        private void SelectPositionsButton_Click(object sender, RoutedEventArgs e)
         {
-            this.DragMove();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                selectedPositionsCsvPath = openFileDialog.FileName;
+                // Aktualizuj Label z wybraną ścieżką
+                // Walidacja struktury pliku
+                if (ValidateCsvFile(selectedPositionsCsvPath, isDocument: false))
+                {
+                    MessageBox.Show("Plik został poprawnie załadowany!", "Poprawny format", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Niepoprawnie wczytany plik!", "Błąd formatu", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                }
+            }
         }
 
-        public void ValidateCsvFile(string filePath)
+        public bool ValidateCsvFile(string filePath, bool isDocument)
         {
             var config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                HasHeaderRecord = true, 
+                HasHeaderRecord = true,
             };
 
             using (var reader = new StreamReader(filePath))
@@ -54,53 +87,36 @@ namespace Profisys_Zadanie.ImportExport
             {
                 csv.Read();
                 csv.ReadHeader();
-                string[] headerRow = csv.HeaderRecord; 
+                string[] headerRow = csv.HeaderRecord;
 
-                if (!IsValidHeaderForDocument(headerRow) && !IsValidHeaderForDocumentItems(headerRow))
+                bool isValid;
+                if (isDocument)
                 {
-                    MessageBox.Show("Niewłaściwy format pliku CSV", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
-                    return; 
+                    isValid = IsValidHeaderForDocument(headerRow);
+                }
+                else
+                {
+                    isValid = IsValidHeaderForDocumentItems(headerRow);
                 }
 
-                while (csv.Read())
+                if (!isValid)
                 {
-                    Document record = new Document();
+                    var missingHeaders = isDocument ?
+                        GetMissingHeaders(headerRow, new[] { "Id", "Type", "Date", "FirstName", "LastName", "City" }) :
+                        GetMissingHeaders(headerRow, new[] { "DocumentID", "Ordinal", "Name", "Quantity", "Price", "TaxRate" });
 
-                    try
-                    {
-                        record.Id = uint.Parse(csv.GetField("Id"));
-                        record.Type = csv.GetField("Type");
-                        record.Date = DateTime.Parse(csv.GetField("Date"));
-                        record.FirstName = csv.GetField("FirstName");
-                        record.LastName = csv.GetField("LastName");
-                        record.City = csv.GetField("City");
-
-                        if (string.IsNullOrEmpty(record.Type) || string.IsNullOrEmpty(record.FirstName) || string.IsNullOrEmpty(record.LastName) || string.IsNullOrEmpty(record.City))
-                        {
-                            MessageBox.Show("Błąd wiersza CSV: niektóre pola są puste.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
-                            continue;
-                        }
-
-                        Documents.Add(record);
-                    }
-                    catch (FormatException ex)
-                    {
-                        
-                        MessageBox.Show($"Błąd formatu danych: {ex.Message}", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
-                        continue; 
-                    }
-                    catch (Exception ex)
-                    {
-                        
-                        MessageBox.Show($"Nieoczekiwany błąd: {ex.Message}", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
-                        continue; 
-                    }
-
-                    // Jeżeli wiersz przeszedł walidację, możesz go teraz użyć do dalszego przetwarzania
+                    MessageBox.Show("Nagłówki nieprawidłowe. Brakujące nagłówki: " + string.Join(", ", missingHeaders), "Błąd walidacji", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
 
+                return isValid;
             }
         }
+
+        private IEnumerable<string> GetMissingHeaders(string[] headers, string[] requiredHeaders)
+        {
+            return requiredHeaders.Where(requiredHeader => !headers.Contains(requiredHeader, StringComparer.OrdinalIgnoreCase));
+        }
+
 
         private bool IsValidHeaderForDocument(string[] headers)
         {
@@ -112,6 +128,17 @@ namespace Profisys_Zadanie.ImportExport
         {
             var requiredHeaders = new[] { "DocumentID", "Ordinal", "Name", "Quantity", "Price", "TaxRate" };
             return requiredHeaders.All(requiredHeader => headers.Contains(requiredHeader, StringComparer.OrdinalIgnoreCase));
+        }
+
+        private void ExitAppBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
 
     }
